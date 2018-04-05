37af5e46138ebb1b11aa3c565989357f7bbdead3
KatarzynaWojtowicz <wojtowicz.katarzyna.anna@gmail.com>
2018-04-05 11:15:44 +0200

    Refactor

---
+++ homebudget/src/main/java/pl/homebudget/app/homebudget/to/ExpenseTo.java
@@ -0,0 +1,10 @@
+package pl.homebudget.app.homebudget.to;
+
+public class ExpenseTo {
+	private String nameExpense;
+	private int count;
+	private double price;
+	private String date;
+	private boolean done;
+
+}
---
+++ homebudget/src/main/java/pl/homebudget/app/homebudget/to/UserTo.java
@@ -0,0 +1,51 @@
+package pl.homebudget.app.homebudget.to;
+
+public class UserTo {
+	private String login;
+	private String username;
+	private String userLastName;
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getUserLastName() {
+		return userLastName;
+	}
+
+	public void setUserLastName(String userLastName) {
+		this.userLastName = userLastName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	private String password;
+	private String email;
+
+}
