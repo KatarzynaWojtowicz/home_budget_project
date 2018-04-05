018b20b293eeb1721a29ecb377969633d2acf654
KatarzynaWojtowicz <wojtowicz.katarzyna.anna@gmail.com>
2018-04-05 11:31:42 +0200

    Created ExpenseTo and CategoryTo class

---
+++ homebudget/src/main/java/pl/homebudget/app/homebudget/to/CategoryTo.java
@@ -0,0 +1,14 @@
+package pl.homebudget.app.homebudget.to;
+
+public class CategoryTo {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
--- homebudget/src/main/java/pl/homebudget/app/homebudget/to/ExpenseTo.java
+++ homebudget/src/main/java/pl/homebudget/app/homebudget/to/ExpenseTo.java
@@ -6,5 +6,53 @@ public class ExpenseTo {
 	private double price;
 	private String date;
 	private boolean done;
+	private CategoryTo category;
 
+	public String getNameExpense() {
+		return nameExpense;
+	}
+
+	public void setNameExpense(String nameExpense) {
+		this.nameExpense = nameExpense;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public boolean isDone() {
+		return done;
+	}
+
+	public void setDone(boolean done) {
+		this.done = done;
+	}
+
+	public CategoryTo getCategory() {
+		return category;
+	}
+
+	public void setCategory(CategoryTo category) {
+		this.category = category;
+	}
 }
